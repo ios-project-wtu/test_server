c6d31030e945f362862d87b112898595352f3578
AllenStig <futianda@user.noreply.gitee.com>
2019-07-20 10:51:00 +0800

    初始化项目

---
+++ test000/test000/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test000
+//
+//  Created by Allenstig on 2019/7/20.
+//  Copyright © 2019 Allenstig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
